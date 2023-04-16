a25ebc9c033ea09f6a3344a3482359c51761962a
ShehzadQadri <63380192+ShehzadQadri@users.noreply.github.com>
2023-04-16 17:20:19 +0500

    MongoDB setup with schema defined

--- controller/contactController.js
+++ controller/contactController.js
@@ -1,14 +1,17 @@
+import asyncHandler from "express-async-handler"
+import { contactModel } from "../models/contactModel"
+
 //@desc get all contacts
 //@route Get /api/contacts
 //@access public
-const getContacts = (req, res) => {
+const getContacts = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "Get all contacts" })
-}
+})
 
 //@desc create contacts
 //@route Post /api/contacts
 //@access public
-const createContact = (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
 
     console.log(`the request body is`, req.body)
     const { name, email, phone } = req.body;
@@ -17,28 +20,28 @@ const createContact = (req, res) => {
         throw new Error("all fields are required")
     }
     res.status(201).json({ message: "Create contacts" })
-}
+})
 
 //@desc get contact
 //@route Get /api/contacts/:id
 //@access public
-const getContact = (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Get Contact for ${req.params.id}` });
-}
+})
 
 //@desc update contact
 //@route Put /api/contacts/:id
 //@access public
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update Contact for ${req.params.id}` });
-}
+})
 
 //@desc delete contact
 //@route delete /api/contacts/:id
 //@access public
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Deleted Contact for ${req.params.id}` });
-}
+})
 
 
 export {
