f302ac0c00518032c23e8663fa4984cbc6d0d85d
shehzadqadri <shehzad.developer92@gmail.com>
2023-05-08 17:45:24 +0500

    updated my complete crud operation

--- controller/contactController.js
+++ controller/contactController.js
@@ -1,90 +1,92 @@
-import asyncHandler from "express-async-handler"
-import { contactModel as Contact } from "../models/contactModel.js"
+import asyncHandler from "express-async-handler";
+import { contactModel as Contact } from "../models/contactModel.js";
 
 //@desc get all contacts
 //@route Get /api/contacts
 //@access private
 const getContacts = asyncHandler(async (req, res) => {
-    const contact = await Contact.find(user_id, req.user.id);
-    res.status(200).json(contact);
-})
+  const contact = await Contact.find({ user_id: req.user.id });
+  return res.status(200).json(contact);
+});
 
 //@desc create contacts
 //@route Post /api/contacts
 //@access private
 const createContact = asyncHandler(async (req, res) => {
-    const { name, email, phone } = req.body;
-    if (!name || !email || !phone) {
-        res.status(400)
-        throw new Error("all fields are required")
-    }
-    const contact = await Contact.create({
-        name,
-        email,
-        phone,
-        user_id: req.user.id,
-    })
-    res.status(201).json(contact)
-})
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400).json({ error: "All fields are required" });
+  }
+  const contact = await Contact.create({
+    name,
+    email,
+    phone,
+    user_id: req.user.id,
+  });
+  res.status(201).json(contact);
+});
 
 //@desc get contact
 //@route Get /api/contacts/:id
 //@access private
 const getContact = asyncHandler(async (req, res) => {
-    if ((req.params.id).length === 24) {
-        const contact = await Contact.findById(req.params.id);
-        contact ?
-            res.status(200).json(contact) :
-            res.status(404); throw new Error("Contact not found")
-    } else {
-        res.status(404)
-        throw new Error("id is invalid, check length and try again")
-    }
-})
+  if (req.params.id.length === 24) {
+    const contact = await Contact.findById(req.params.id);
+    contact ? res.status(200).json(contact) : res.status(404);
+    throw new Error("Contact not found");
+  } else {
+    res.status(404);
+    throw new Error("id is invalid, check length and try again");
+  }
+});
 
 //@desc update contact
 //@route Put /api/contacts/:id
 //@access private
 const updateContact = asyncHandler(async (req, res) => {
-    if ((req.params.id).length === 24) {
-        const updatedContact = await Contact.findByIdAndUpdate(
-            req.params.id,
-            req.body,
-            { new: true }
-        );
+  const contact = await Contact.findById(req.params.id);
 
-        updatedContact ?
-            res.status(200).json(updatedContact) :
-            res.status(404); throw new Error("Contact not found")
+  // 403 for user cannot update other users contact
+  if (contact.user_id.toString() !== req.user.id) {
+    res.status(403);
+    throw new Error("User don't have permission to update other user contacts");
+  }
 
-    } else {
-        res.status(404)
-        throw new Error("id is invalid, check length and try again")
-    }
-})
+  if (req.params.id.length === 24) {
+    const updatedContact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    updatedContact ? res.status(200).json(updatedContact) : res.status(404);
+    throw new Error("Contact not found");
+  } else {
+    res.status(404);
+    throw new Error("id is invalid, check length and try again");
+  }
+});
 
 //@desc delete contact
 //@route delete /api/contacts/:id
 //@access public
 const deleteContact = asyncHandler(async (req, res) => {
-    if ((req.params.id).length === 24) {
-        const deletedContact = await Contact.findByIdAndDelete(req.params.id);
+  const contact = await Contact.findById(req.params.id);
 
-        deletedContact ?
-            res.status(200).json(deletedContact) :
-            res.status(404); throw new Error("Contact not found")
+  if (contact.user_id.toString() !== req.user.id) {
+    res.status(403);
+    throw new Error("User don't have permission to update other user contacts");
+  }
 
-    } else {
-        res.status(404)
-        throw new Error("id is invalid, check length and try again")
-    }
-})
+  if (req.params.id.length === 24) {
+    const deletedContact = await Contact.findByIdAndDelete(req.params.id);
 
+    deletedContact ? res.status(200).json(deletedContact) : res.status(404);
+    throw new Error("Contact not found");
+  } else {
+    res.status(404);
+    throw new Error("id is invalid, check length and try again");
+  }
+});
 
-export {
-    getContacts,
-    getContact,
-    createContact,
-    updateContact,
-    deleteContact
-}
+export { getContacts, getContact, createContact, updateContact, deleteContact };
--- models/contactModel.js
+++ models/contactModel.js
@@ -1,26 +1,28 @@
 import mongoose from "mongoose";
 
-const contactSchema = mongoose.Schema({
+const contactSchema = mongoose.Schema(
+  {
     user_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: "User",
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
     },
     name: {
-        type: String,
-        required: [true, "Please add the contact name."],
+      type: String,
+      required: [true, "Please add the contact name."],
     },
     email: {
-        type: String,
-        required: [true, "Please add the contact email address."],
+      type: String,
+      required: [true, "Please add the contact email address."],
     },
     phone: {
-        type: String,
-        required: [true, "Please add the contact phone number."],
-    }
-}, { timestamps: true })
+      type: String,
+      required: [true, "Please add the contact phone number."],
+    },
+  },
+  { timestamps: true }
+);
 
-const contactModel = mongoose.model("Contact", contactSchema)
+const contactModel = mongoose.model("Contact", contactSchema);
 
-
-export { contactModel }
+export { contactModel };
